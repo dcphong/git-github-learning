0c9d6ceafae4eedfe70d47224fe41b1a36c1f0f8
dcphong <doanchanphong0610@gmail.com>
2025-03-10 01:40:08 +0700

    testing created new branch

---
+++ index.js
@@ -0,0 +1,16 @@
+const addSum = (a, b) => {
+  return a + b;
+};
+
+const addMul = (a, b) => {
+  return a * b;
+};
+
+const addMod = (a, b) => {
+  return a % b;
+};
+
+module.exports = {
+  addSum,
+  addMul,
+};
